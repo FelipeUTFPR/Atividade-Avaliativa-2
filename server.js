fec4c9a93bcb7e20e63e9b21c46aadd311e5cfc8
FelipeUTFPR <fps2887@gmail.com>
2021-12-16 20:13:15 -0300

    Commit #0

---
+++ server.js
@@ -0,0 +1,8 @@
+const nodemailer = require('nodemailer');
+const storage = require('node-persist');
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
